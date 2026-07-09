6a173054e17ce05e86dc30120724466bfb4bec5e
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-09 15:48:13 -0400

    Creado formulario con radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedFruit, setSelectedFruit] = useState("banana");
+
+  const fruits = ["apple", "banana", "cherry"];
+
+  const handleChange = (e) => {
+    setSelectedFruit(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    alert(`The selected fruit is ${selectedFruit}`);
+    e.preventDefault();
+  };
 
   return (
     <>
       <section className="container my-2">
-        <p>Layout</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Select your favorite fruit</label>
+            {fruits.map((fruit) => (
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="fruit"
+                    checked={selectedFruit === fruit}
+                    value={fruit}
+                    onChange={handleChange}
+                  ></input>
+                  {fruit}
+                </label>
+              </div>
+            ))}
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <hr />
+
+        <p>Selected fruit: {selectedFruit}</p>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
